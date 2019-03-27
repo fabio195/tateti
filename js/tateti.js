84c629dae6581c4252e6d22d0bb1c0ecf29e89a4
fabio195 <fabionahumunoz2@gmail.com>
2019-03-27 16:53:06 -0300

    guardados nombres de los jugadores

--- js/tateti.js
+++ js/tateti.js
@@ -5,29 +5,33 @@ var tabla = [
 ];
 
 var jugador = 1;
+var nombreJugador1;
+var nombreJugador2;
 
 function marcar(cuadradito) {
-    console.log('se hace clic en el boton ', cuadradito);
-
-    console.log('jugador es: ' + jugador)
-
     if (this.tabla[cuadradito] != 0) {
         deshabilitarBoton(cuadradito);
     } else {
         tabla[cuadradito] = jugador;
     }
-    console.log('turno jugador: ', jugador)
-
     jugador = (jugador == 1) ? 2 : 1
-
-    console.log('la tabla es: ', tabla)
     refreshUI()
+}
+
+function guardarJugador1() {
+    nombreJugador1 = document.getElementById('nombJugador1').value;
+}
+
+function guardarJugador2() {
+    nombreJugador2 = document.getElementById('nombJugador2').value;
 }
 
 function refreshUI() {
     tabla.forEach((val, index, table) => {
         document.getElementById("" + index).innerText = "" + val
     });
+    document.getElementById('nombJugador1').innerText = "" + this.nombreJugador1
+    document.getElementById('nombJugador2').innerText = "" + this.nombreJugador2
 }
 
 function deshabilitarBoton(idBoton) {
